9cd58bdd1345e75ec31dc6dc909ef1dcc8538fea
Alexey Melezhik <melezhik@gmail.com>
2015-08-23 19:21:21 +0000

    pod + small fixed

--- mongodb-http/lib/swat/mongodb.pm
+++ mongodb-http/lib/swat/mongodb.pm
@@ -1,3 +1,37 @@
 package swat::mongodb;
 our $VERSION = 'v0.1.0';
 1;
+
+__END__
+
+
+=head1 SYNOPSIS
+
+Swat tests for mongodb http interface. This is simple test suit could be used in various CI processes, f.e. when running tests in travis.
+
+=head1 INSTALL
+
+    perl Makefile.PL
+    make
+    make test
+    make install
+
+=head1 USAGE
+
+Install mongodb
+
+    $ sudo apt-get install mongodb
+
+Run mongodb with --rest
+
+    $ sudo /etc/init.d/mongodb start
+
+Run swat tests
+
+    $ swat swat::mongodb 127.0.0.1:28017
+
+
+
+=for html <img href="../../../images/mongodb-http.png">
+
+
