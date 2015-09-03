26b80140ee50d24974eb9b19e7462c13a492e47a
Alexey Melezhik <melezhik@gmail.com>
2015-09-03 16:25:09 +0000

    swat-my

--- nginx/share/startup.bash
+++ nginx/share/startup.bash
@@ -4,6 +4,7 @@ export nginx_dest_dir
 export nginx_configure_flags
 export port
 export safe_project
+export nginx_skip_install
 
 if  bash    $safe_project/install-nginx.bash 1>/tmp/install-nginx.log 2>&1; then
 :
