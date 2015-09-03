0ef4859ac03b2d54af72f9ac852d3bbc6e9bd07c
Alexey Melezhik <melezhik@gmail.com>
2015-09-03 13:26:51 +0000

    nginx-benchmark-project-up

---
+++ nginx/share/cleanup.bash
@@ -0,0 +1,2 @@
+test -f $nginx_dest_dir/usr/local/nginx/logs/nginx.pid && kill `cat $nginx_dest_dir/usr/local/nginx/logs/nginx.pid`
+
--- nginx/share/fastcgi/hook.pm
+++ nginx/share/fastcgi/hook.pm
@@ -1,4 +1,4 @@
-my $cmd = "plackup --server FCGI --port 9000  --pid /tmp/fscgi.pid -D  --host 127.0.0.1 app.psgi";
+my $cmd = "plackup --server FCGI --port 9000  --pid /tmp/app.pid -D  --host 127.0.0.1 app.psgi";
 ok(1,"run $cmd");
 
 `$cmd`;
