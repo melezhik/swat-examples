1d96b260df958eb065a79714322318e902637da2
Alexey Melezhik <melezhik@gmail.com>
2015-09-04 10:07:39 +0000

    installer

--- nginx/share/install-nginx.bash
+++ nginx/share/install-nginx.bash
@@ -1,9 +1,16 @@
 cdir=`pwd`
 
-test -f $nginx_dest_dir/usr/local/nginx/logs/nginx.pid && kill `cat $nginx_dest_dir/usr/local/nginx/logs/nginx.pid`
 
-if [ -n "${nginx_skip_install}" ]; then
+if test -n "${nginx_reinstall}"; then
+    test -f $nginx_dest_dir/usr/local/nginx/logs/nginx.pid && kill `cat $nginx_dest_dir/usr/local/nginx/logs/nginx.pid`
+    rm -rf $nginx_dest_dir/usr/local/nginx/sbin/nginx
+fi
+
+
+if test -s $nginx_dest_dir/usr/local/nginx/sbin/nginx  ; then
+
     :
+
 else
 
     rm -rf $nginx_dest_dir
@@ -12,21 +19,21 @@ else
     mkdir -p /tmp/nginx-source/
     cd /tmp/nginx-source 
     
-    wget --debug --verbose $nginx_source_url || exit 1
+    curl  $nginx_source_url -o data.tar.gz || exit 1
     
     tar -xzf  *.tar.gz
     cd nginx-*
     ./configure $nginx_configure_flags
     make
     make install DESTDIR=$nginx_dest_dir
-    
+
     cd $cdir
     
+    cp $safe_project/nginx.conf $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
+    perl -i -p -e "s{%port%}[$port]" $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
+
     
 fi 
 
-cp $safe_project/nginx.conf $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
-perl -i -p -e "s{%port%}[$port]" $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
 
-$nginx_dest_dir/usr/local/nginx/sbin/nginx -p $nginx_dest_dir/usr/local/nginx/
 
--- nginx/share/startup.bash
+++ nginx/share/startup.bash
@@ -4,12 +4,15 @@ export nginx_dest_dir
 export nginx_configure_flags
 export port
 export safe_project
-export nginx_skip_install
+export nginx_reinstall
 
-if  bash    $safe_project/install-nginx.bash 1>/tmp/install-nginx.log 2>&1; then
+if  bash  $safe_project/install-nginx.bash 1>/tmp/install-nginx.log 2>&1; then
 :
 else
  cat /tmp/install-nginx.log
  exit 1
 fi
 
+
+test -f $nginx_dest_dir/usr/local/nginx/logs/nginx.pid && kill `cat $nginx_dest_dir/usr/local/nginx/logs/nginx.pid`
+$nginx_dest_dir/usr/local/nginx/sbin/nginx -p $nginx_dest_dir/usr/local/nginx/
