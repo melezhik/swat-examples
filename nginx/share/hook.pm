865618eba182cff12afd31c572ac0070ed43bb42
Alexey Melezhik <melezhik@gmail.com>
2015-09-04 12:26:29 +0000

    update=doc

--- nginx/share/hook.pm
+++ nginx/share/hook.pm
@@ -1,6 +1,6 @@
 sub install_nginx {
     $pid = open(INSTALOG, "bash $project/install-nginx.bash |")  or die "Couldn't fork: $!\n";
-    local $| = 1;
+    $| = 1;
     while (<INSTALOG>) {
        diag($_);
     }
@@ -12,6 +12,7 @@ if (! -e "$ENV{nginx_dest_dir}/usr/local/nginx/sbin/nginx"){
     install_nginx()
 }
 
+BAIL_OUT("failed to istall nginx") unless -e '/tmp/nginx-source/install.ok';
 
 1;
 
--- nginx/share/install-nginx.bash
+++ nginx/share/install-nginx.bash
@@ -6,22 +6,24 @@ mkdir -p /tmp/nginx-source/
 
 cd /tmp/nginx-source 
 
-curl -s $nginx_source_url -o data.tar.gz || exit 1
+curl -D - -s $nginx_source_url -o data.tar.gz || exit 1
 
 tar -xzf  *.tar.gz
 cd nginx-*
-./configure $nginx_configure_flags
-make
-make install DESTDIR=$nginx_dest_dir
+./configure "${nginx_configure_flags}" || exit 1
+make || exit 1
+make install DESTDIR=$nginx_dest_dir || exit 1
 
 cd $cdir
 
 echo patching nginx.conf
 echo port: $port
 
-cp $safe_project/nginx.conf $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
-perl -i -p -e "s{%port%}[$port]" $nginx_dest_dir/usr/local/nginx/conf/nginx.conf
+cp $safe_project/nginx.conf $nginx_dest_dir/usr/local/nginx/conf/nginx.conf || exit 1
+perl -i -p -e "s{%port%}[$port]" $nginx_dest_dir/usr/local/nginx/conf/nginx.conf || exit 1
 
-$nginx_dest_dir/usr/local/nginx/sbin/nginx -p $nginx_dest_dir/usr/local/nginx/
+$nginx_dest_dir/usr/local/nginx/sbin/nginx -p $nginx_dest_dir/usr/local/nginx/ || exit 1
 
+
+touch /tmp/nginx-source/install.ok
 
