37a126df2e26ef6d07bd8372b5bbaf6b06e21b4c
Alexey Melezhik <melezhik@gmail.com>
2015-09-07 14:46:31 +0000

    moved

--- lighttpd/share/cleanup.bash
+++
@@ -1,7 +0,0 @@
-export lighttpd_source_url
-export lighttpd_dest_dir
-export port
-export project
-export lighttpd_reinstall
-
-bash $project/stop-lighttpd.bash
--- lighttpd/share/stop-nginx.bash
+++
@@ -1,1 +0,0 @@
-test -f $nginx_dest_dir/usr/local/nginx/logs/nginx.pid && kill `cat $nginx_dest_dir/usr/local/nginx/logs/nginx.pid`
